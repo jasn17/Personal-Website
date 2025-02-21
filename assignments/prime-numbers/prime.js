f39c9d3d8c4881f4ff1ad38671c585424044665b
jasn17 <Jas0n17L1n@gmail.com>
2025-02-21 12:59:23 -0500

    added color change and acsending + decsending order feature

--- assignments/prime-numbers/prime.js
+++ assignments/prime-numbers/prime.js
@@ -46,14 +46,54 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
   }  
 
-function generatePrimeList(limit) {
-
-}
-
-function generateNonPrimeList(limit) {
-
-}
-
 function calculateSum(list) {
 
 }
+
+
+function toggleSort(listId) {
+    // get the listId element
+    let listElement = document.getElementById(listId);
+    // get the list of numbers
+    let items = Array.from(listElement.children).map(li => parseInt(li.textContent));
+
+    // toggle sorting order
+    isAscending = !isAscending;
+    items.sort((a, b) => isAscending ? a - b : b - a);
+
+    // update the list
+    updateList(listId, items);
+}
+
+// sort list
+function addSortButtons(listId) {
+    const list = ["primeList", "nonPrimeList"];
+
+    lists.forEach(listId => {
+        let sortBtn = document.getElementById(listId + "Sort");
+        sortBtn.textContent = "Sort";
+        sortBtn.classList.add(sort-btn);
+        sortBtn.onclick = () => { togglesort(listId)};
+        document.getElementById(listId).parentNode.appendChild(sortBtn);
+    });
+}
+
+window.onload = addSortButtons;
+
+function changeColors() {
+    const colors = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff"];
+    currColorIndex = 0;
+
+    // chnages the colors of the list every 5 seconds
+    setInterval(() => {
+        document.querySelectorAll(".list").forEach(list => {
+            list.style.transition = "background-color 1s ease-in-out";
+            list.style.backgroundColor = colors[currColorIndex];
+        });
+
+        // switches to the next color every 5 secodns
+        currColorIndex = (currColorIndex + 1) % colors.length;
+    }, 5000);
+}
+
+changeListColors();
