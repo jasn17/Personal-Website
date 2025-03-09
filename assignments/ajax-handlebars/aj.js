98aa698b30fa471de369b66f258bfbe5756e020a
jasn17 <Jas0n17L1n@gmail.com>
2025-03-09 11:44:34 -0400

    updated json

--- assignments/ajax-handlebars/aj.js
+++ assignments/ajax-handlebars/aj.js
@@ -1,2 +1,7 @@
-var outRequest = new XMLHttpRequest();
-ourRequqest.open('GET', )
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://raw.githubusercontent.com/jasn17/Personal-Website/refs/heads/main/assignments/ajax-handlebars/textbook.json');
+ourRequest.onload = function() {
+    var textbooks = ourRequest.responseText;
+    console.log(textbooks[0]);
+};
+ourRequest.send();
