da69d201388946c824c55633f7800a3fd594f239
jasn17 <Jas0n17L1n@gmail.com>
2025-03-09 13:29:04 -0400

    added phtoots

--- assignments/ajax-handlebars/aj.js
+++ assignments/ajax-handlebars/aj.js
@@ -1,7 +1,23 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://raw.githubusercontent.com/jasn17/Personal-Website/refs/heads/main/assignments/ajax-handlebars/textbook.json');
 ourRequest.onload = function() {
-    var textbooks = ourRequest.responseText;
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        var textbooks = JSON.parse(ourRequest.responseText);
+        createHTML(textbooks);
+    }
+    else {
+        console.log('We connected to the server, but it returned an error.');
+    }
     console.log(textbooks[0]);
 };
+
+ourRequest.onerror = function() {
+    console.log('Connection error');
+}
+
 ourRequest.send();
+ 
+function createHTML(bookData) {
+    console.log('testing...');
+    console.log(bookData);
+}
