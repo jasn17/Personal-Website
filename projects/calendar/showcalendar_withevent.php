4b23ec5d4e59bb6661a53c9b9e5d7f9276dab129
jasn17 <Jas0n17L1n@gmail.com>
2025-04-08 14:57:49 -0400

    indent

--- projects/calendar/showcalendar_withevent.php
+++ projects/calendar/showcalendar_withevent.php
@@ -15,118 +15,117 @@ $firstDayArray = getdate($start);
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php echo "Calendar: ".$firstDayArray['month']." ".$firstDayArray['year']; ?></title>
+  	<title><?php echo "Calendar: ".$firstDayArray['month']." ".$firstDayArray['year']; ?></title>
 </head>
 <body>
-  <div id="navbar-container"></div>
-  <script>
-    fetch('../../navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading navbar:', error));
-  </script>
-
-  <h1>Select a Month/Year Combination</h1>
-  <div>
-    <a href="about.html" title="About the team">About the Team</a>
-    <a href="proposal.php" title="Proposal for the final">Proposal for the final</a>
-    <a href="showcalendar_withevent.php" title="Calendar">Calendar</a>
-  </div>
-  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <select name="month">
-      <?php
-      $months = Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-      for ($x = 1; $x <= count($months); $x++) {
-        echo "<option value=\"$x\"";
-        if ($x == $month) {
-          echo " selected";
-        }
-        echo ">".$months[$x-1]."</option>";
-      }
-      ?>
-    </select>
-    <select name="year">
-      <?php
-      for ($x = 1990; $x <= 2025; $x++) {
-        echo "<option";
-        if ($x == $year) {
-          echo " selected";
-        }
-        echo ">$x</option>";
-      }
-      ?>
-    </select>
-    <button type="submit" name="submit" value="submit">Go!</button>
-  </form>
-  <br>
-  <?php
-  $days = Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
-  echo "<table><tr>\n";
-  foreach ($days as $day) {
-    echo "<th>".$day."</th>\n";
-  }
-  for ($count = 0; $count < (6*7); $count++) {
-    $dayArray = getdate($start);
-    if (($count % 7) == 0) {
-      if ($dayArray['mon'] != $month) {
-        break;
-      } else {
-        echo "</tr><tr>\n";
-      }
-    }
-    if ($count < $firstDayArray['wday'] || $dayArray['mon'] != $month) {
-      echo "<td>&nbsp;</td>\n";
-    } else {
-		$event_title = "";
-		$mysqli = mysqli_connect("localhost", "root", "", "cal");
-		$chkEvent_sql = "SELECT event_title FROM calendar_events WHERE
-		month(event_start) = '".$month."' AND
-		dayofmonth(event_start) = '".$dayArray['mday']."'
-		AND year(event_start) = '".$year."' ORDER BY event_start";
-		$chkEvent_res = mysqli_query($mysqli, $chkEvent_sql)
-		or die(mysqli_error($mysqli));
-
-		if (mysqli_num_rows($chkEvent_res) > 0) {
-		while ($ev = mysqli_fetch_array($chkEvent_res)) {
-			$event_title .= stripslashes($ev['event_title'])."<br>";
-		}
-		} else {
-		$event_title = "";
-		}
-
-		echo "<td><a href=\"javascript:eventWindow('event.php?m=".$month.
-		"&amp;d=".$dayArray['mday']."&amp;y=$year');\">".$dayArray['mday']."</a>
-		<br>".$event_title."</td>\n";
-
-		unset($event_title);
-
-		$start += ADAY;
-    }
-  }
-  echo "</tr></table>";
-
-  //close connection to MySQL
-  mysqli_close($mysqli);
-  ?>
-
-  <script type="text/javascript">
-    function eventWindow(url) {
-		event_popupWin = window.open(url, 'event', 'resizable=yes, scrollbars=yes, toolbar=no,width=400,height=400');
-		event_popupWin.opener = self;
-    }
-  </script>
-
-  <div id="footer-container"></div>
-  <script>
-    fetch('../../footer.html')
-	.then(response => response.text())
+	<div id="navbar-container"></div>
+	<script>
+	fetch('../../navbar.html')
+		.then(response => response.text())
 		.then(data => {
-		document.getElementById('footer-container').innerHTML = data;
+		document.getElementById('navbar-container').innerHTML = data;
 		})
-    .catch(error => console.error('Error loading footer:', error));
-  </script>
+		.catch(error => console.error('Error loading navbar:', error));
+	</script>
 
+  	<h1>Select a Month/Year Combination</h1>
+	<div>
+		<a href="about.html" title="About the team">About the Team</a>
+		<a href="proposal.php" title="Proposal for the final">Proposal for the final</a>
+		<a href="showcalendar_withevent.php" title="Calendar">Calendar</a>
+	</div>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<select name="month">
+		<?php
+		$months = Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+		for ($x = 1; $x <= count($months); $x++) {
+			echo "<option value=\"$x\"";
+			if ($x == $month) {
+			echo " selected";
+			}
+			echo ">".$months[$x-1]."</option>";
+		}
+		?>
+		</select>
+		<select name="year">
+		<?php
+		for ($x = 1990; $x <= 2025; $x++) {
+			echo "<option";
+			if ($x == $year) {
+			echo " selected";
+			}
+			echo ">$x</option>";
+		}
+		?>
+		</select>
+		<button type="submit" name="submit" value="submit">Go!</button>
+	</form>
+	<br>
+	<?php
+	$days = Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+	echo "<table><tr>\n";
+	foreach ($days as $day) {
+		echo "<th>".$day."</th>\n";
+	}
+	for ($count = 0; $count < (6*7); $count++) {
+		$dayArray = getdate($start);
+		if (($count % 7) == 0) {
+		if ($dayArray['mon'] != $month) {
+			break;
+		} else {
+			echo "</tr><tr>\n";
+		}
+		}
+		if ($count < $firstDayArray['wday'] || $dayArray['mon'] != $month) {
+		echo "<td>&nbsp;</td>\n";
+		} else {
+			$event_title = "";
+			$mysqli = mysqli_connect("localhost", "root", "", "cal");
+			$chkEvent_sql = "SELECT event_title FROM calendar_events WHERE
+			month(event_start) = '".$month."' AND
+			dayofmonth(event_start) = '".$dayArray['mday']."'
+			AND year(event_start) = '".$year."' ORDER BY event_start";
+			$chkEvent_res = mysqli_query($mysqli, $chkEvent_sql)
+			or die(mysqli_error($mysqli));
+
+			if (mysqli_num_rows($chkEvent_res) > 0) {
+			while ($ev = mysqli_fetch_array($chkEvent_res)) {
+				$event_title .= stripslashes($ev['event_title'])."<br>";
+			}
+			} else {
+			$event_title = "";
+			}
+
+			echo "<td><a href=\"javascript:eventWindow('event.php?m=".$month.
+			"&amp;d=".$dayArray['mday']."&amp;y=$year');\">".$dayArray['mday']."</a>
+			<br>".$event_title."</td>\n";
+
+			unset($event_title);
+
+			$start += ADAY;
+		}
+	}
+	echo "</tr></table>";
+
+	//close connection to MySQL
+	mysqli_close($mysqli);
+	?>
+
+	<script type="text/javascript">
+		function eventWindow(url) {
+			event_popupWin = window.open(url, 'event', 'resizable=yes, scrollbars=yes, toolbar=no,width=400,height=400');
+			event_popupWin.opener = self;
+		}
+	</script>
+
+	<div id="footer-container"></div>
+	<script>
+		fetch('../../footer.html')
+		.then(response => response.text())
+			.then(data => {
+			document.getElementById('footer-container').innerHTML = data;
+			})
+		.catch(error => console.error('Error loading footer:', error));
+	</script>
 </body>
 </html>
